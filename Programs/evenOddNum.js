6bbd6ba28beabe1c3f991a286ac369a7fb770cc4
Abhie <abhikore5577@gmail.com>
2023-11-08 12:06:10 +0530

    Code added

---
+++ Programs/evenOddNum.js
@@ -0,0 +1,8 @@
+var num=25;
+
+if(num%2==0){
+    console.log(num,"is even number")
+}
+else{
+    console.log(num,"is odd number")
+}
---
+++ Programs/squareRoot.js
@@ -0,0 +1,15 @@
+let num=25;
+
+let sqrt=Math.sqrt(num)
+
+console.log("Square Root of", num ,"is :", sqrt)
+
+
+let num1=36;
+let sqrt1= num1** 0.5;
+console.log("Square Root of", num1 ,"is :", sqrt1)
+
+let num2=49;
+let sqrt2=Math.pow(num2, 0.5);
+console.log("Square Root of", num2 ,"is :", sqrt2)
+
