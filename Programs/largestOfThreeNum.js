b4a0c987a73a489f66cf6bbbd47a4ba2856945d1
Abhie <abhikore5577@gmail.com>
2023-11-10 08:22:32 +0530

    two files added

---
+++ Programs/largestOfThreeNum.js
@@ -0,0 +1,17 @@
+//Find the largest number among three numbers
+
+let a=10;
+b=20;
+c=30;
+
+if(a>b && a>c){
+    console.log("Large rnum is :", a);
+}
+
+else if(b>a && b>c){
+    console.log("Large rnum is :", b);
+}
+
+else{
+    console.log("Large rnum is :", c);
+}
---
+++ Programs/swapVariables.js
@@ -0,0 +1,14 @@
+//Program to Swap Two Variables values
+
+
+let a=50;
+let b=25;
+
+let temp=a;
+a=b;
+b=temp;
+
+console.log("a ="+a);
+console.log("b ="+b)
+
+
