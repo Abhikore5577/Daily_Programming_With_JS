4b11ac8d864bdc425262983c3010121d2d5f8cbd
Abhie <abhikore5577@gmail.com>
2023-11-13 17:40:10 +0530

    kmToMile

---
+++ Programs/convertKMToMile.js
@@ -0,0 +1,12 @@
+//WAP to convert kilometer to miles
+
+function kmToMile(a){
+    return a*0.6213;
+}
+
+function mileToKm(b){
+    return b*1.6093;
+}
+
+console.log(kmToMile(5))
+console.log(mileToKm(3.1064999))
