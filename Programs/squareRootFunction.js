ff5ea352e57b6459cd50c973d56c5301b4c396ee
Abhie <abhikore5577@gmail.com>
2023-11-14 18:40:12 +0530

    sqrt added

---
+++ Programs/squareRootFunction.js
@@ -0,0 +1,18 @@
+//Square root by using function
+
+function sqroot(num){
+    return Math.sqrt(num)
+}
+console.log(sqroot(25))
+
+
+function sqroot1(num, expo){
+    return Math.pow(num, expo)
+}
+console.log(sqroot1(36, 0.5))
+
+
+function sqroot2(num){
+    return num**0.5;
+}
+console.log(sqroot2(49))
